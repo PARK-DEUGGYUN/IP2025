8159ea438fc344851241ed1c8dc75fa9ea942f1c
PARK-DEUGGYUN <emrrbs2315@gmail.com>
2025-09-19 16:30:24 +0900

    Update chap144.py

--- chap144.py
+++ chap144.py
@@ -3,11 +3,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img=cv2.imread('152339.jpeg')
-k=5
-blur = cv2.medianBlur(img,(k,k),0)
-median = cv2.medianBlur(img,k)
+
+k=9
+blur = cv2.GaussianBlur(img,(k,k),0)
+median = cv2.medianBlur(img, k)
+
 plt.subplot(121),plt.imshow(median),plt.title('3x3')
-plt.xticks([]),plt.yticks([])
 plt.subplot(122),plt.imshow(blur),plt.title('gaussian blur')
-plt.xticks([]),plt.yticks([])
 plt.show()
